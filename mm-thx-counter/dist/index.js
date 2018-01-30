4ffb3f5eb03919eedc117ec83d480ad9bb666c35
依田　剛 <takeshi@YodakeMBA-4.local>
2018-01-30 23:08:37 +0900

    フォルダリネーム

---
+++ mm-thx-counter/dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  return res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  return console.log('Example app listening on port 3000!');
+});
+//# sourceMappingURL=index.js.map
